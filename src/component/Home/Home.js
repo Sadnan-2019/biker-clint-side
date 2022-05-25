f37d31118413387d68ca8d59556bc83882c2f481
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-05-26 03:41:25 +0600

    footer done

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Business from "../Business/Business";
 import Category from "../Category/Category";
+import Footer from "../Footer/Footer";
 import Gallery from "../Gallery/Gallery";
 import Review from "../Review/Review";
 import Team from "../Team/Team";
@@ -19,6 +20,7 @@ const Home = () => {
       <Review></Review>
       <Gallery></Gallery>
       <Business></Business>
+      <Footer></Footer>
     </div>
   );
 };
