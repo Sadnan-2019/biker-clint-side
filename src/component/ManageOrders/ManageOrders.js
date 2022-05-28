aa3f23641543b4823a3d6dfcfe980db49a26222f
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-05-28 21:44:19 +0600

    manage orders  done

--- src/component/ManageOrders/ManageOrders.js
+++ src/component/ManageOrders/ManageOrders.js
@@ -12,32 +12,35 @@ const ManageOrders = () => {
           return (
                     <div>
                            <div class="overflow-x-auto">
-        <h3 className="text-2xl text-center font-bold mb-4">Manage Tools</h3>
+        <h3 className="text-2xl text-center font-bold mb-4">Manage Orders</h3>
         <table class="table w-full">
           <thead>
             <tr>
               <th>Serial No</th>
-              <th>Name</th>
-              <th>Short-description</th>
-              <th>Available quantity</th>
-              <th>Minimum quantity</th>
+              <th>Tools Name</th>
               <th>Price</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Quentity</th>
+              <th>Phone</th>
               <th></th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {orders.map((tool, i) => (
+            {orders.map((order, i) => (
               <tr>
                 <th>{i + 1}</th>
-                <td>{tool.name}</td>
-                <td>{tool.shortdescription}</td>
-                <td>{tool.availablequantity}</td>
+                <td>{order.toolsName}</td>
+                <td>${order.price}</td>
+              
 
-                <td>{tool.minimumquantity}</td>
-                <td>${tool.price}</td>
+                <td>{order.customerName}</td>
+                <td>{order.customerEmail}</td>
+                <td>{order.totalQuentity}</td>
+                <td>{order.phone}</td>
                 <td>
-                  <img src={tool.img} alt="" className="w-[20%]" />
+                  <img src={order.img} alt="" className="w-[20%]" />
                 </td>
                 
               </tr>
