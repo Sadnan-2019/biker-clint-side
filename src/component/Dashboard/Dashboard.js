401539b8edc82fd199a2aca97d62d3864f7ce912
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-05-29 19:52:57 +0600

    admin privillige

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const [admin] = useAdmin(user);
   return (
     <div class="drawer drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content ">
-                <h3 className="text-2xl font-bold"> Your Dashboard</h3>
-                <Outlet></Outlet>
-       
+        <h3 className="text-2xl font-bold"> Your Dashboard</h3>
+        <Outlet></Outlet>
       </div>
       <div class="drawer-side">
         <label for="dashboard-sidebar" class="drawer-overlay"></label>
@@ -19,9 +23,11 @@ const Dashboard = () => {
           <li>
             <Link to="/dashboard/add-review">Add Review</Link>
           </li>
-          <li>
-            <Link to="/dashboard/all-users">All Users</Link>
-          </li>
+          {
+            admin &&  <li>
+              <Link to="/dashboard/all-users">All Users</Link>
+            </li>
+          }
           <li>
             <Link to="/dashboard/profile">My Profile</Link>
           </li>
